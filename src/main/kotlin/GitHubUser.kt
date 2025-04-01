a9f79ffc0334c9955120dbaec40109b87512c7ea
a.taghipour <alitaghipour80@gmail.com>
2025-04-01 18:07:12 +0330

    Adds followers and following count to github user

--- src/main/kotlin/GitHubUser.kt
+++ src/main/kotlin/GitHubUser.kt
@@ -6,7 +6,8 @@ data class GitHubUser(
     @SerializedName("id") val id: Int,
     @SerializedName("name") val name: String,
     @SerializedName("bio") val bio: String,
-    @SerializedName("company") val company: String,
+    @SerializedName("followers") val followers: Int,
+    @SerializedName("following") val following: Int,
     @SerializedName("repos_url") val reposUrl: String,
     var publicRepositories: List<GitHubRepository>,
 )
